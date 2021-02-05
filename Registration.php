1c75b2a820bb09bfa3b97cc27243445476b2db91
Shariar Sijan Alvi <75942553+ssalvi8586@users.noreply.github.com>
2021-02-05 14:25:02 +0600

    Add files via upload

---
+++ Registration.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Registration</title>
+</head>
+<body>
+Registration 
+<br>
+<form>
+<table>
+<tr>
+<td>First Name: </td><td><input type="text" id="a" name="aname"></td>
+</tr>
+<tr><td>Email:</td><td> <input type="text" id="b" name="bname"></td>
+</tr>
+<tr>
+<td>User Name: </td><td><input type="text" id="c" name="cname"></td>
+</tr>
+<tr><td>Password:</td><td> <input type="password" id="d" name="dname"><td>
+</tr>
+<tr><td>Confirm Password:</td> <td> <input type="password" id="e" name="ename"><td>
+</tr>
+</table>
+Gender:<br>
+<input type="radio" id="male" name="gender" value="male">
+Male
+<input type="radio" id="female" name="gender" value="female">
+Female
+<input type="radio" id="other" name="gender" value="other">
+Other
+<br>Date of Birth:<br>
+<input type="date" id="b" name="b">
+<br>
+<table><tr><td>
+<input type ="submit" value="Submit"></td>
+<td><input type ="reset" value="Reset"></td>
+</tr>
+</table>
+</form>
+</body>
+</html>
