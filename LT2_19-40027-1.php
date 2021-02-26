7efc9e64f48dfaa4d4abbc59272351b4f8020117
Shariar Sijan Alvi <75942553+ssalvi8586@users.noreply.github.com>
2021-02-26 11:47:17 +0600

    Add files via upload
    Lab Task 2. Late upload due to inconvenience as I messaged earlier

---
+++ LT2_19-40027-1.php
@@ -0,0 +1,104 @@
+<?php
+    $validateName="";
+    $validateEmail="";
+    $validateUserName="";
+    $validatePassword="";
+    $validateConfirmPassword="";
+    $validateGender="";
+    $validateDateOfBirth="";
+
+    if($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+            $name = $_REQUEST['name'];
+            $email = $_REQUEST['email'];
+            $userName = $_REQUEST['userName'];
+            $password = $_REQUEST['pass'];
+            $confirmPassword = $_REQUEST['confPass'];
+
+            if(empty($name) || strlen($name)<5){
+                $validateName="Invalid Name!!! Please Enter a Valid Name";
+            }
+            else{
+                $validateName=$name;
+            }
+            if(empty($email) || !preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$/i", $email)){
+                $validateEmail="Invalid Email!!! Please Enter a Valid Email";
+            }
+            else{
+                $validateEmail=$email;
+            }
+            if(empty($userName) || !preg_match('/[a-zA-Z0-9._]{5,}$/', $userName)){
+                $validateUserName="Invalid Username!!! Please enter a valid Username";
+            }
+            else{
+                $validateUserName=$userName;
+            }
+            if(empty($password) || strlen($password)<6 || !preg_match("/(?=.*[@#$%^&+=]).*$/", $password)){
+                $validatePassword = "!!!Password must contain atleast 6 characters including atleast 1 special character!!!";
+            }
+            else{
+                $validatePassword = "Password accepted!!!";
+            }
+            if(!empty($confirmPassword) && $password == $confirmPassword){
+                $validateConfirmPassword = "Correct Password!!!";
+            }
+            else{
+                $validateConfirmPassword = "Incorrect Password!!!";
+            }
+            
+
+                 
+    }
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Registration</title>
+</head>
+<body>
+    <h1>Registration</h1>
+    <table>
+    <form action="<?php echo $_SERVER["PHP_SELF"] ?>" method="POST">
+    <tr>
+        <td>Name: </td><td><input type="text" id="name" name="name"></td>
+        <?php echo $validateName ?><br>
+    </tr>
+    <tr>
+        <td>Email:</td><td> <input type="text" id="id" name="email"></td>
+        <?php echo $validateEmail ?><br>
+    </tr>  
+    <tr>  
+        <td>User Name: </td><td><input type="text" id="userName" name="userName"></td>
+        <?php echo $validateUserName ?><br>
+    </tr>
+    <tr>  
+        <td>Password:</td><td> <input type="password" id="pass" name="pass"><td> 
+        <?php echo $validatePassword ?><br>
+    </tr>
+    <tr>  
+        <td>Confirm Password:</td> <td> <input type="password" id="confPass" name="confPass"><td> 
+        <?php echo $validateConfirmPassword ?><br>
+    </tr>
+    <tr>
+    <td>Gender <br>
+    <input type="radio" id="male" name="gender" value="male">
+    Male
+    <input type="radio" id="female" name="gender" value="female">
+    Female
+    <input type="radio" id="other" name="gender" value="other">
+    Other</td>
+    </tr>      
+    <tr>
+    <td>Date of Birth <br>  
+    <input type="date" id="birthday" name="birthday"></td>
+    </tr> 
+    <tr>
+    <td><input type="submit" value="SUBMIT">
+    <input type="reset" value="RESET"></td>
+    </tr>
+    </form>
+    </table>
+</body>
+</html>
